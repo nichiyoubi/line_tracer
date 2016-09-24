64b3d78c5990c47e0dd23471b2e00ca5c031df1a
Masanori Usami <nichiyoubinoshisya@gmail.com>
2016-09-24 11:09:19 +0900

    AllTestsクラスを追加

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import jp.topse.patterns.*;
+import jp.topse.line_tracer.*;
+
+@RunWith(Suite.class)
+@SuiteClasses({ StateTest.class })
+public class AllTests {
+
+}
